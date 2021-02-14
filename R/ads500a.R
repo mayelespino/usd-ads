78f6554b5dfa553f8fa3579660bf8735ceef983a
Mayel Espino <mayel.espino@gmail.com>
2021-02-13 23:53:26 -0800

    Added graph and summary examples.

--- R/ads500a.R
+++ R/ads500a.R
@@ -1,35 +1,56 @@
-install.packages("dyplr")
+install.packages("janitor")
+library(janitor)
+
+tabyl(Book2, fuelType) %>%
+  adorn_percentages("col") %>%
+  adorn_pct_formatting(digits = 4)
+
+#Good Graph!
+install.packages("CGPfunctions")
+library(CGPfunctions)
+PlotXTabs(Book2, fuelType, co2TailpipeGpm, "side") 
+PlotXTabs2(Book2, fuelType, co2TailpipeGpm) 
+
+?PlotXTabs2
+
+
+
+# ERROR
+# https://cran.r-project.org/web/packages/vtree/vignettes/vtree.html
+#install.packages("vtree")
+#install.packages("mime")
+#library(mime)
+#library(vtree)
+#vtree(fuelType,"fuelType")
+
+# Not very useful in this case
+xtabs(~ fuelType + co2TailpipeGpm, data = Book2)
+
+library(CGPfunctions)
+PlotXTabs(Book2, "fuelType", "co2TailpipeGpm") 
+help("PlotXTabs")
+
 library(dplyr)
+my_summary <- Book2 %>%
+  count(fuelType, co2TailpipeGpm, sort = TRUE)
 
-summary(Book2)
-attach(Book2)
+my_summary
 
-boxplot(comb08)
-boxplot(barrels08)
-boxplot(co2TailpipeGpm)
-boxplot(cylinders)
-boxplot(emissionscat)
+library(ggplot2)
+ggplot(my_summary)
 
-plot(barrels08 ~ comb08)
-plot(barrels08 ~ co2TailpipeGpm)
-plot(comb08 ~ co2TailpipeGpm)
-plot(cylinders ~ co2TailpipeGpm)
+
+install.packages("dypl")
+library(dplyr)
+glimpse(Book2)
+
+library(tibble)
+glimpse(Book2$fuelType)
+
+library(dplyr)
+Book2 %>%
+  distinct(fuelType)
 
 Book2 %>%
-  select(comb08, barrels08, co2TailpipeGpm, cylinders) %>%
-  summary()
+  count(fuelType)
 
-Book2 %>%
-  select(comb08, barrels08, co2TailpipeGpm, cylinders) %>%
-  boxplot()
-
-
-Book2 %>%
-  select(comb08, barrels08, co2TailpipeGpm, cylinders) %>%
-  filter(cylinders != "") %>%
-  summary()
-
-Book2 %>%
-  select(comb08, barrels08, co2TailpipeGpm, cylinders) %>%
-  filter(cylinders != "") %>%
-  boxplot()
